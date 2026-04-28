128a97e4f4b8cb5cef3f6b7124f60af90a709608
TeTeago <55003392+TeTeago@users.noreply.github.com>
2026-04-28 19:59:38 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+// Nome: Tiago Falco França
+// Matrícula: 1261942635
+
+public class Main {
+
+    public static void imprimirArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+       
+        int[] numeros = {10, 20, 30, 40, 50};
+
+        imprimirArray(numeros);
+    }
+
+}
