3f1085d665919a5414736cd298c9682d2d26c063
TeTeago <55003392+TeTeago@users.noreply.github.com>
2026-04-28 20:05:56 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,17 +3,24 @@
 
 public class Main {
 
-    public static void imprimirArray(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
+    public static int somarArray(int[] numeros) {
+        int soma = 0;
+
+        for (int i = 0; i < numeros.length; i++) {
+            soma += numeros[i];
         }
+
+        return soma;
     }
 
     public static void main(String[] args) {
-       
-        int[] numeros = {10, 20, 30, 40, 50};
 
-        imprimirArray(numeros);
+        int[] array1 = {1, 2, 3, 4, 5};
+        System.out.println("Soma: " + somarArray(array1));
+
+        int[] array2 = {10, 20, 30};
+        System.out.println("Soma: " + somarArray(array2));
+
     }
 
 }
